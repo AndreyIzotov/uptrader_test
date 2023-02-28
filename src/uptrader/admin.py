dd0b1fca26b06d7e53aa17488f63b636427f634c
Андрей Изотов <andrej.izotoff@yandex.ru>
2023-02-28 18:28:22 +0300

    done

--- src/uptrader/admin.py
+++ src/uptrader/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item, Menu
+
+
+@admin.register(Item)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent')
+    list_filter = ('menu',)
+    fieldsets = (
+        ('Add new item', {
+            'description': "Parent should be a menu or item",
+            'fields': (('menu', 'parent'), 'title', 'slug')
+            }),
+            )
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
